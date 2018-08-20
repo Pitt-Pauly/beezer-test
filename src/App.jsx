45c616862212425bc53ab0292b45e665e4b38667
Pitt Pauly <pitt.pauly@gmail.com>
2018-08-20 19:09:08 +0100

    prettier table + spinner and some style components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
+import styled, { keyframes } from 'styled-components';
 import logo from './logo.svg';
-import './App.css';
 import UserTable from './UserTable';
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Logo = styled.img`
+  animation: ${rotate360} infinite 20s linear;
+  height: 80px;
+`;
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  background-color: #222;
+  height: 100px;
+  padding: 20px;
+  color: white;
+  width: 100%;
+`;
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
+    <Header className="App-header">
+      <Logo src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Beezer Coding Test</h1>
+    </Header>
     <UserTable />
   </div>
 );
--- src/UserTable.jsx
+++ src/UserTable.jsx
@@ -1,62 +1,32 @@
 // @flow
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import firebase from './firebase';
 import 'firebase/database';
+import spinner from './Facebook-1.1s-134px.svg';
+import './UserTable.css';
 
-const CombinedTable = (props: { accounts: Object, users: Object }) => {
-  const { accounts, users } = props;
-  return (
-    <div className="UserTable">
-      <table>
-        <thead>
-          <tr>
-            <th>user id</th>
-            <th>name</th>
-            <th>account</th>
-            <th>apps</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(users).map(id => {
-            const currentAccount = users[id].account;
+const Wrapper = styled.div`
+  width: 90%;
+  margin-top: 25px;
+  margin-right: auto;
+  margin-bottom: 25px;
+  margin-left: auto;
+`;
 
-            let accountDetails = (
-              <tr>
-                <th>{id}</th>
-                <th>{users[id].name}</th>
-                <th colSpan="2">Account info not available</th>
-              </tr>
-            );
-            if (
-              accounts[currentAccount] !== null &&
-              accounts[currentAccount] !== undefined
-            ) {
-              accountDetails = (
-                <tr>
-                  <th>{id}</th>
-                  <th>{users[id].name}</th>
-                  <th>{currentAccount}</th>
-                  <th>
-                    <ul>
-                      {Object.keys(accounts[currentAccount].apps).map(app => (
-                        <li id={app}>
-                          {accounts[currentAccount].apps[app].title}
-                        </li>
-                      ))}
-                    </ul>
-                  </th>
-                </tr>
-              );
-            }
-            return accountDetails;
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+const LoadingWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
 
-/* eslint-disable no-console */
+const Spinner = styled.img`
+  height: 2.5em;
+`;
+
 class UserTable extends Component<
   void,
   { apiData: { accounts: Object, users: Object }, initComplete: boolean }
@@ -76,14 +46,9 @@ class UserTable extends Component<
       .once('value')
       .then(snapshot => {
         this.setState({
-          apiData: snapshot.val()
+          apiData: snapshot.val(),
+          initComplete: true
         });
-        this.setState({ initComplete: true });
-        const {
-          apiData: { accounts, users }
-        } = this.state;
-        console.log(Object.entries(accounts));
-        console.log(users);
       });
   }
 
@@ -92,20 +57,80 @@ class UserTable extends Component<
       apiData: { accounts, users },
       initComplete
     } = this.state;
-    console.log(typeof users);
 
-    let content = <h1> Loading user data </h1>;
+    let content = (
+      <LoadingWrapper>
+        <h2> Loading user data </h2>
+        <Spinner src={spinner} className="Spinner" alt="loading spinner" />
+      </LoadingWrapper>
+    );
     if (initComplete) {
       content = (
-        <div className="tableWrapper">
-          <CombinedTable users={users} accounts={accounts} />
-        </div>
+        <Wrapper className="UserTable">
+          <table>
+            <caption>
+              <h2>User Information</h2>
+            </caption>
+            <thead>
+              <tr>
+                <th>
+                  <h2>User ID</h2>
+                </th>
+                <th>
+                  <h2>Name</h2>
+                </th>
+                <th>
+                  <h2>Account</h2>
+                </th>
+                <th>
+                  <h2>Apps</h2>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(users).map(id => {
+                const currentAccount = users[id].account;
+
+                let accountDetails = (
+                  <tr>
+                    <th>{id}</th>
+                    <th>{users[id].name}</th>
+                    <th colSpan="2">Account info not available</th>
+                  </tr>
+                );
+                if (
+                  accounts[currentAccount] !== null &&
+                  accounts[currentAccount] !== undefined
+                ) {
+                  accountDetails = (
+                    <tr>
+                      <th>{id}</th>
+                      <th>{users[id].name}</th>
+                      <th>{currentAccount}</th>
+                      <th>
+                        <ul>
+                          {Object.keys(accounts[currentAccount].apps).map(
+                            app => (
+                              <li id={app}>
+                                {accounts[currentAccount].apps[app].title}
+                              </li>
+                            )
+                          )}
+                        </ul>
+                      </th>
+                    </tr>
+                  );
+                }
+                return accountDetails;
+              })}
+            </tbody>
+          </table>
+        </Wrapper>
       );
     }
 
     return content;
   }
 }
-/* eslint-enable no-console */
 
 export default UserTable;
